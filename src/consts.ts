4b326856ddea714dcf0b4617581818004adb7993
Kazuya Izumi <shikibu9419@gmail.com>
2023-08-18 08:53:27 +0900

    Create first page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "泉和哉 / Kazuya Izumi";
+export const SITE_DESCRIPTION = "Kazuya Izumi's WIP website";
